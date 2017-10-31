a5402f0cd8c493f1a0a0238242d2400e37584c67
coding4funrocks <thefitgeekgirl@gmail.com>
2017-10-31 18:42:23 +0100

    Unit test for renderer model to verify that PropertyChanged is raised when property is set.

---
+++ Mills.UnitTests/Models/RendererModelTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mills.Models;
+using System.ComponentModel;
+using System.Windows.Media;
+using Mills.UnitTests.Eventing;
+
+namespace Mills.UnitTests.Models
+{
+    [TestClass]
+    public class RendererModelTests
+    {
+        [TestMethod]
+        public void CurrentPlayerColor_EventSubscriber_PropertyChangedRaised()
+        {
+            // Arrange
+            var rendererModel = new RendererModel(null);
+            
+            var mockEventSubscriber = new MockEventSubscriber();
+            rendererModel.PropertyChanged += mockEventSubscriber.Handle;
+
+            // Act
+            rendererModel.CurrentPlayerColor = new SolidColorBrush() { Color = Colors.Black };
+
+            // Assert
+            Assert.AreEqual(1, mockEventSubscriber.HitCount);
+        }
+
+        [TestMethod]
+        public void UserMessage_EventSubscriber_PropertyChangedRaised()
+        {
+            // Arrange
+            var rendererModel = new RendererModel(null);
+
+            var mockEventSubscriber = new MockEventSubscriber();
+            rendererModel.PropertyChanged += mockEventSubscriber.Handle;
+
+            // Act
+            rendererModel.UserMessage = "Test message";
+
+            // Assert
+            Assert.AreEqual(1, mockEventSubscriber.HitCount);
+        }
+    }
+}
